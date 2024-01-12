9470f38a1ad4879889afba8e0a93781ff87fb598
sodiumKlorida <ezra.gilang.e9@gmail.com>
2024-01-12 19:35:18 +0700

    hore ngoding

---
+++ view/content/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style/index.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="logo">beecara</div>
+        <div class="menu">
+            <div class="menu1">menu1</div>
+            <div class="menu1">menu2</div>
+        </div>
+    </div>
+</body>
+</html>
