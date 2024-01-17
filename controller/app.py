908b8009af272e32a7a953a2735199975dbc84c6
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2024-01-17 17:45:21 +0700

    Create App file.

--- controller/app.py
+++ controller/app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('../../view/pages/index.html')
+    return render_template('index.html')
 
 
 @app.route('/login')
 def login():
-    return render_template('../../view/pages/login.html')
+    return render_template('login.html')
 
 
 @app.route('/register')
 def register():
-    return render_template('../../view/pages/register.html')
+    return render_template('register.html')
